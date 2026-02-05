c3e2ae5f036ad6b756aff21cefcf9b922b47ca14
vedant-nage <vedantnage2@gmail.com>
2026-02-05 09:04:10 +0530

    remove comments

--- urlshortener/server.js
+++ urlshortener/server.js
@@ -2,18 +2,18 @@ import express from "express";
 import { nanoid } from "nanoid";
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 
 const urlDatabase = {};
 
 app.use(express.json());
-//middleware
+
 app.use((req, res, next) => {
-  console.log( req.method, req.url);//this is use to check ,request is working or not 
+  console.log( req.method, req.url);
   next();
 });
-// here we send the post request like the https://youtube.com and if we send srequest then we will get the response
+
 app.post("/shorten", (req, res) => {
   const { originalUrl } = req.body;
   if (!originalUrl) return res.status(400).json({ error: "URL required" });
@@ -21,10 +21,9 @@ app.post("/shorten", (req, res) => {
   const id = nanoid(6); 
   urlDatabase[id] = originalUrl;
   console.log(urlDatabase);
-  res.json({ shortUrl: `http://localhost:${PORT}/${id}` });
+  res.json({ shortUrl: `http://localhost:${port}/${id}` });
 });
 
-// Route to redirect to the original url
 app.get("/:id", (req, res) => {
   const originalUrl = urlDatabase[req.params.id];
   if (!originalUrl) return res.status(404).send("URL not found");
@@ -33,5 +32,5 @@ app.get("/:id", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`URL shortener running at http://localhost:${PORT}`);
+  console.log(`URL shortener running at http://localhost:${port}`);
 });
